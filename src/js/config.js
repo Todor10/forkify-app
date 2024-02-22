8da096192066c70bd735d96855a268df92461f12
Todor10 <todorovicn2002@gmail.com>
2024-02-22 16:37:43 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8f7635be-a037-4397-a5d2-61388fc303b2';
+export const MODAL_CLOSE_SEC = 2.5;
